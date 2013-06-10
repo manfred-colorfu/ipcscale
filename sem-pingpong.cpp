791aedb165c496ba09b3c0ccb059b65adc0b6031
Manfred Spraul <manfred@colorfullife.com>
2013-06-10 19:58:20 +0200

    First commit

---
+++ sem-pingpong.cpp
@@ -0,0 +1,317 @@
+/*
+ * sem-pingpong.cpp, parallel sysv sem pingpong
+ *
+ * Copyright (C) 1999, 2001, 2005, 2008, 2013 by Manfred Spraul.
+ *	All rights reserved except the rights granted by the GPL.
+ *
+ * Redistribution of this file is permitted under the terms of the GNU 
+ * General Public License (GPL) version 3 or later.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <pthread.h>
+
+#ifdef __sun
+	 #include <sys/pset.h> /* P_PID, processor_bind() */
+#endif
+
+#undef VERBOSE
+
+//////////////////////////////////////////////////////////////////////////////
+
+static enum {
+	WAITING,
+	RUNNING,
+	STOPPED,
+} volatile g_state = WAITING;
+
+unsigned long long *g_results;
+int *g_svsem_ids;
+int *g_svsem_nrs;
+pthread_t *g_threads;
+
+struct taskinfo {
+	int svsem_id;
+	int svsem_nr;
+	int threadid;
+	int cpubind;
+	int sender;
+};
+
+void bind_cpu(int cpunr)
+{
+#if __sun
+	int ret;
+	ret = processor_bind(P_PID, getpid(), cpunr, NULL);
+	if (ret == -1) {
+		perror("bind_thread:processor_bind");
+		printf(" Binding to cpu %d failed.\n", cpunr);
+	}
+#else
+	int ret;
+	cpu_set_t cpus;
+	cpu_set_t v;
+	CPU_ZERO(&cpus);
+	CPU_SET(cpunr, &cpus);
+	pthread_t self;
+
+	self = pthread_self();
+
+	ret = pthread_setaffinity_np(self, sizeof(cpus), &cpus);
+	if (ret < 0) {
+		printf("pthread_setaffinity_np failed for thread %p with errno %d.\n",
+				(void*)self, errno);
+	}
+
+	ret = pthread_getaffinity_np(self, sizeof(v), &v);
+	if (ret < 0) {
+		printf("pthread_getaffinity_np() failed for thread %p with errno %d.\n",
+				(void*)self, errno);
+		fflush(stdout);
+	}
+	if (memcmp(&v, &cpus, sizeof(cpus) != 0)) {
+		printf("Note: Actual affinity does not match intention: got 0x%08lx, expected 0x%08lx.\n",
+			(unsigned long)v.__bits[0], (unsigned long)cpus.__bits[0]);
+	}
+	fflush(stdout);
+#endif
+}
+#define DATASIZE	8
+
+void* worker_thread(void *arg)
+{
+	struct taskinfo *ti = (struct taskinfo*)arg;
+	unsigned long long rounds;
+	int ret;
+
+	bind_cpu(ti->cpubind);
+#ifdef VERBOSE
+	printf("thread %d: sysvsem %8d, off %8d type %d bound to cpu %d\n",ti->threadid,
+			ti->svsem_id, ti->svsem_nr,
+			ti->sender, ti->cpubind);
+#endif
+	
+	rounds = 0;
+	while(g_state == WAITING) {
+#ifdef __GNUC__
+#if defined(__i386__) || defined (__x86_64__)
+		__asm__ __volatile__("pause": : :"memory");
+#else
+		__asm__ __volatile__("": : :"memory");
+#endif
+#endif
+	}
+
+	if (ti->sender) {
+		struct sembuf sop[1];
+
+		/* 1) insert token */
+		sop[0].sem_num=ti->svsem_nr+0;
+		sop[0].sem_op=1;
+		sop[0].sem_flg=0;
+		ret = semop(ti->svsem_id,sop,1);
+	
+		if (ret != 0) {
+			printf("Initial semop failed, ret %d, errno %d.\n", ret, errno);
+			exit(1);
+		}
+	}
+	while(g_state == RUNNING) {
+		struct sembuf sop[1];
+
+		/* 1) retrieve token */
+		sop[0].sem_num=ti->svsem_nr+ti->sender;
+		sop[0].sem_op=-1;
+		sop[0].sem_flg=0;
+		ret = semop(ti->svsem_id,sop,1);
+		if (ret != 0) {
+			/* EIDRM can happen */
+			if (errno == EIDRM)
+				break;
+
+			/* Some OS do not report EIDRM properly */
+			if (g_state != RUNNING)
+				break;
+			printf("main semop failed, ret %d errno %d.\n", ret, errno);
+			printf(" round %lld sop: num %d op %d flg %d.\n",
+					rounds,
+					sop[0].sem_num, sop[0].sem_op, sop[0].sem_flg);
+			fflush(stdout);
+			exit(1);
+		}
+
+		/* 2) reinsert token */
+		sop[0].sem_num=ti->svsem_nr+1-ti->sender;
+		sop[0].sem_op=1;
+		sop[0].sem_flg=0;
+		ret = semop(ti->svsem_id,sop,1);
+		if (ret != 0) {
+			/* EIDRM can happen */
+			if (errno == EIDRM)
+				break;
+			/* Some OS do not report EIDRM properly */
+			if (g_state != RUNNING)
+				break;
+			printf("main semop failed, ret %d errno %d.\n", ret, errno);
+			printf(" round %lld sop: num %d op %d flg %d.\n",
+					rounds,
+					sop[0].sem_num, sop[0].sem_op, sop[0].sem_flg);
+			fflush(stdout);
+			exit(1);
+		}
+		rounds++;
+	}
+	g_results[ti->threadid] = rounds;
+
+	pthread_exit(0);
+	return NULL;
+}
+
+void init_threads(int cpu, int cpus, int sems, int shared)
+{
+	int ret;
+	struct taskinfo *ti1, *ti2;
+
+	ti1 = (struct taskinfo*)malloc(sizeof(struct taskinfo));
+	ti2 = (struct taskinfo*)malloc(sizeof(struct taskinfo));
+	if (!ti1 || !ti2) {
+		printf("Could not allocate task info\n");
+		exit(1);
+	}
+	if (cpu % sems == 0) {
+		int i;
+		g_svsem_ids[cpu] = semget(IPC_PRIVATE,2*sems,0777|IPC_CREAT);
+		if(g_svsem_ids[cpu] == -1) {
+			printf("sem array create failed.\n");
+			exit(1);
+		}
+		for (i=0;i<sems;i++) {
+			g_svsem_ids[cpu+i] = g_svsem_ids[cpu];
+			g_svsem_nrs[cpu+i] = 2*i;
+		}
+	}
+
+	g_results[cpu] = 0;
+	g_results[cpu+cpus] = 0;
+
+	ti1->svsem_id = g_svsem_ids[cpu];
+	ti1->svsem_nr = g_svsem_nrs[cpu];
+	ti1->threadid = cpu;
+	ti1->cpubind = cpu;
+	ti1->sender = 1;
+	ti2->svsem_id = g_svsem_ids[cpu];
+	ti2->svsem_nr = g_svsem_nrs[cpu];
+	ti2->threadid = cpu+cpus;
+	if (shared) {
+		ti2->cpubind = cpu;
+	} else {
+		ti2->cpubind = cpus+cpu;
+	}
+	ti2->sender = 0;
+
+	ret = pthread_create(&g_threads[ti1->threadid], NULL, worker_thread, ti1);
+	if (ret) {
+		printf(" pthread_create failed with error code %d\n", ret);
+		exit(1);
+	}
+	ret = pthread_create(&g_threads[ti2->threadid], NULL, worker_thread, ti2);
+	if (ret) {
+		printf(" pthread_create failed with error code %d\n", ret);
+		exit(1);
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void do_psem(int queues, int timeout, int shared)
+{
+	unsigned long long totals;
+	int i;
+	int sems = queues; /* No need to test multiple arrays: that part scales linearly */
+
+	g_state = WAITING;
+
+	g_results = (unsigned long long*)malloc(sizeof(unsigned long long)*2*queues);
+	g_svsem_ids = (int*)malloc(sizeof(int)*(queues+sems));
+	g_svsem_nrs = (int*)malloc(sizeof(int)*(queues+sems));
+	g_threads = (pthread_t*)malloc(sizeof(pthread_t)*2*queues);
+	for (i=0;i<queues;i++) {
+		init_threads(i, queues, sems, shared);
+	}
+
+	usleep(10000);
+	g_state = RUNNING;
+	sleep(timeout);
+	g_state = STOPPED;
+	usleep(10000);
+	for (i=0;i<queues;i++) {
+		int res;
+		if (g_svsem_nrs[i] == 0) {
+			res = semctl(g_svsem_ids[i],1,IPC_RMID,NULL);
+			if (res < 0) {
+				printf("semctl(IPC_RMID) failed for %d, errno%d.\n",
+					g_svsem_ids[i], errno);
+			}
+		}
+	}
+	for (i=0;i<2*queues;i++)
+		pthread_join(g_threads[i], NULL);
+
+#ifdef VERBOSE
+	printf("Result matrix:\n");
+#endif
+	totals = 0;
+	for (i=0;i<queues;i++) {
+#ifdef VERBOSE
+		printf("  Thread %3d: %8lld     %3d: %8lld\n",
+				i, g_results[i], i+queues, g_results[i+queues]);
+#endif
+		totals += g_results[i] + g_results[i+queues];
+	}
+	printf("Queues: %d (%s) %lld in %d secs\n", queues, shared ? "intra-cpu" : "inter-cpu",
+			totals, timeout);
+
+	free(g_results);
+	free(g_svsem_ids);
+	free(g_svsem_nrs);
+	free(g_threads);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char **argv)
+{
+	int max_cpus;
+	int timeout;
+	int i;
+
+	printf("sem-pingpong [max cpus] [timeout]\n");
+	if (argc != 3) {
+		printf(" Invalid parameters.\n");
+		printf("\n");
+		printf(" Sem-pingpong create pairs of threads that perform ping/pong with sysv semaphores.\n");
+		printf(" The threads are cpu bound: either to the same cpu or to different cpus.\n");
+		return 1;
+	}
+	max_cpus = atoi(argv[1]);
+	timeout = atoi(argv[2]);
+	/* Intra-cpu */
+	for (i=1;i<=max_cpus;i++) {
+		usleep(10000);
+		do_psem(i, timeout, 1);
+	}
+	/* Inter-cpu */
+	for (i=1;i<=max_cpus/2;i++) {
+		usleep(10000);
+		do_psem(i, timeout, 0);
+	}
+
+}
---
+++ sem-waitzero.cpp
@@ -0,0 +1,484 @@
+/*
+ * sem-waitzero.cpp - sysv scaling test
+ *
+* Copyright (C) 1999, 2001, 2005, 2008, 2013 by Manfred Spraul.
+ *	All rights reserved except the rights granted by the GPL.
+ *
+ * Redistribution of this file is permitted under the terms of the GNU 
+ * General Public License (GPL) version 3 or later.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <pthread.h>
+
+#ifdef __sun
+	 #include <sys/pset.h> /* P_PID, processor_bind() */
+#endif
+
+#define VERBOSE
+#undef VERBOSE
+
+//////////////////////////////////////////////////////////////////////////////
+
+#define DELAY_BUBBLESORT
+
+#ifdef DELAY_LOOP
+
+#define DELAY_LOOPS	20
+
+static volatile int g_numerator = 12345678;
+static volatile int g_denominator = 123456;
+
+unsigned long long do_delay(int loops)
+{
+	unsigned long long sum;
+	int i, j;
+
+	sum = loops;
+	for (i=0;i<loops;i++) {
+		for (j=0;j<DELAY_LOOPS;j++) {
+			sum += g_numerator/g_denominator;
+		}
+	}
+	return sum;
+}
+
+#elif defined (DELAY_BUBBLESORT)
+
+#define BUF_SIZE	12
+static volatile int g_BUF_SIZE	= BUF_SIZE;
+int do_delay(int loops)
+{
+	int sum;
+	int data[64];
+	int i, j, k;
+
+	sum = 0;
+	for (i=0;i<loops;i++) {
+		/* init with reverse order */
+		for(j=0;j<g_BUF_SIZE;j++)
+			data[j]=g_BUF_SIZE-j;
+
+		for(j=g_BUF_SIZE;j>1;j=j-1) {
+			for(k=0;k<j;k++) {
+				if (data[k] > data[k+1]) {
+					int tmp;
+					tmp = data[k];
+					data[k] = data[k+1];
+					data[k+1] = tmp;
+				}
+			}
+		}
+		sum = sum + data[0];
+	}
+	return sum;
+}
+#else
+
+#error Unknown delay operation
+
+#endif
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+#define DELAY_10MS	(10000)
+
+static enum {
+	WAITING,
+	RUNNING,
+	STOPPED,
+} volatile g_state = WAITING;
+
+unsigned long long *g_results;
+int g_svsem_id;
+int g_max_cpus;
+int *g_svsem_nrs;
+pthread_t *g_threads;
+
+struct taskinfo {
+	int svsem_id;
+	int svsem_nr;
+	int threadid;
+	int cpubind;
+	int interleave;
+	int delay;
+};
+
+int get_cpunr(int cpunr, int interleave)
+{
+	int off = 0;
+	int ret = 0;
+
+#ifdef VERBOSE
+	printf("get_cpunr %p: cpunr %d max_cpu %d interleave %d.\n",
+		(void*)pthread_self(), cpunr, g_max_cpus, interleave);
+#endif
+
+	while (cpunr>0) {
+		ret += interleave;
+		if (ret >=g_max_cpus) {
+			off++;
+			ret = off;
+		}
+		cpunr--;
+	}
+#ifdef VERBOSE
+	printf("get_cpunr %p: result %d.\n", (void*)pthread_self(), ret);
+#endif
+	return ret;
+}
+
+void bind_cpu(int cpunr)
+{
+	int ret;
+#if __sun
+	ret = processor_bind(P_PID, getpid(), cpunr, NULL);
+	if (ret == -1) {
+		perror("bind_thread:processor_bind");
+		printf(" Binding to cpu %d failed.\n", cpunr);
+	}
+#else
+	cpu_set_t cpus;
+	cpu_set_t v;
+	CPU_ZERO(&cpus);
+	CPU_SET(cpunr, &cpus);
+	pthread_t self;
+
+	self = pthread_self();
+
+	ret = pthread_setaffinity_np(self, sizeof(cpus), &cpus);
+	if (ret < 0) {
+		printf("pthread_setaffinity_np failed for thread %p with errno %d.\n",
+				(void*)self, errno);
+	}
+
+	ret = pthread_getaffinity_np(self, sizeof(v), &v);
+	if (ret < 0) {
+		printf("pthread_getaffinity_np() failed for thread %p with errno %d.\n",
+				(void*)self, errno);
+		fflush(stdout);
+	}
+	if (memcmp(&v, &cpus, sizeof(cpus) != 0)) {
+		printf("Note: Actual affinity does not match intention: got 0x%08lx, expected 0x%08lx.\n",
+			(unsigned long)v.__bits[0], (unsigned long)cpus.__bits[0]);
+	}
+	fflush(stdout);
+#endif
+}
+
+void* worker_thread(void *arg)
+{
+	struct taskinfo *ti = (struct taskinfo*)arg;
+	unsigned long long rounds;
+	int ret;
+	int cpu = get_cpunr(ti->cpubind, ti->interleave);
+
+	bind_cpu(cpu);
+#ifdef VERBOSE
+	printf("thread %d: sysvsem %8d, off %8d bound to cpu %d\n",ti->threadid,
+			ti->svsem_id, ti->svsem_nr,cpu);
+#endif
+	
+	rounds = 0;
+	while(g_state == WAITING) {
+#ifdef __GNUC__
+#if defined(__i386__) || defined (__x86_64__)
+		__asm__ __volatile__("pause": : :"memory");
+#else
+		__asm__ __volatile__("": : :"memory");
+#endif
+#endif
+	}
+
+	while(g_state == RUNNING) {
+		struct sembuf sop[1];
+
+		/* 1) check if the semaphore value is 0 */
+		sop[0].sem_num=ti->svsem_nr;
+		sop[0].sem_op=0;
+		sop[0].sem_flg=0;
+		ret = semop(ti->svsem_id,sop,1);
+		if (ret != 0) {
+			/* EIDRM can happen */
+			if (errno == EIDRM)
+				break;
+
+			printf("main semop failed, ret %d errno %d.\n", ret, errno);
+
+			/* Some OS do not report EIDRM properly */
+			if (g_state != RUNNING)
+				break;
+			printf(" round %lld sop: num %d op %d flg %d.\n",
+					rounds,
+					sop[0].sem_num, sop[0].sem_op, sop[0].sem_flg);
+			fflush(stdout);
+			exit(1);
+		}
+		if (ti->delay)
+			do_delay(ti->delay);
+		rounds++;
+	}
+	g_results[ti->threadid] = rounds;
+
+	pthread_exit(0);
+	return NULL;
+}
+
+void init_threads(int cpu, int cpus, int delay, int interleave)
+{
+	int ret;
+	struct taskinfo *ti;
+
+	ti = (struct taskinfo*)malloc(sizeof(struct taskinfo));
+	if (!ti) {
+		printf("Could not allocate task info\n");
+		exit(1);
+	}
+	if (cpu == 0) {
+		int i;
+		g_svsem_id = semget(IPC_PRIVATE,cpus,0777|IPC_CREAT);
+		if(g_svsem_id == -1) {
+			printf("sem array create failed.\n");
+			exit(1);
+		}
+		for (i=0;i<cpus;i++)
+			g_svsem_nrs[i] = i;
+	}
+
+	g_results[cpu] = 0;
+
+	ti->svsem_id = g_svsem_id;
+	ti->svsem_nr = g_svsem_nrs[cpu];
+	ti->threadid = cpu;
+	ti->cpubind = cpu;
+	ti->interleave = interleave;
+	ti->delay = delay;
+
+	ret = pthread_create(&g_threads[ti->threadid], NULL, worker_thread, ti);
+	if (ret) {
+		printf(" pthread_create failed with error code %d\n", ret);
+		exit(1);
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+unsigned long long do_psem(int cpus, int timeout, int delay, int interleave)
+{
+	unsigned long long totals;
+	int i;
+	int res;
+
+	g_state = WAITING;
+
+	g_results = (unsigned long long*)malloc(sizeof(unsigned long long)*cpus);
+	g_svsem_nrs = (int*)malloc(sizeof(int)*cpus);
+	g_threads = (pthread_t*)malloc(sizeof(pthread_t)*cpus);
+
+	for (i=0;i<cpus;i++)
+		init_threads(i, cpus, delay, interleave);
+
+	usleep(DELAY_10MS);
+	g_state = RUNNING;
+	sleep(timeout);
+	g_state = STOPPED;
+	usleep(DELAY_10MS);
+
+	res = semctl(g_svsem_id,1,IPC_RMID,NULL);
+	if (res < 0) {
+		printf("semctl(IPC_RMID) failed for %d, errno%d.\n",
+			g_svsem_id, errno);
+	}
+
+	for (i=0;i<cpus;i++)
+		pthread_join(g_threads[i], NULL);
+
+#ifdef VERBOSE
+	printf("Result matrix:\n");
+#endif
+	totals = 0;
+	for (i=0;i<cpus;i++) {
+#ifdef VERBOSE
+		printf("  Thread %3d: %8lld\n",
+				i, g_results[i]);
+#endif
+		totals += g_results[i];
+	}
+	printf("Cpus %d, interleave %d delay %d: %lld in %d secs\n",
+			cpus, interleave, delay,
+			totals, timeout);
+
+	free(g_results);
+	free(g_svsem_nrs);
+	free(g_threads);
+
+	return totals;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+int *decode_commastring(const char *str)
+{
+	int i, len, count, pos;
+	int *ret;
+
+	len = strlen(str);
+	count = 1;
+	for (i=1;i<len;i++) {
+		if (str[i] == ',')
+			count++;
+	}
+	ret = (int*)malloc(sizeof(int)*(count+1));
+	if (!ret) {
+		printf("Could not allocate memory for decoding parameters.\n");
+		exit(1);
+	}
+
+	pos = 0;
+	for (i=0;i<count;i++) {
+		ret[i] = 0;
+		while (str[pos] != ',') {
+			ret[i] = ret[i]*10 + str[pos]-'0';
+			pos++;
+			if (pos >= len)
+				break;
+		}
+		pos++;
+	}
+	ret[count] = 0;
+	return ret;
+}
+//////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char **argv)
+{
+	int timeout;
+	unsigned long long totals, max_totals;
+	int *interleaves;
+	int *cpus;
+	int fastest;
+	int i, j, k;
+
+	printf("sem-waitzero [timeout] <interleave1,interleave2> <cpu1,cpu2>\n");
+
+	if (argc < 2) {
+		printf(" Invalid parameters.\n");
+		printf("\n");
+		printf(" Sem-waitzero performs parallel 'test-for-zero' sysv semaphore operations.\n");
+		printf(" Each thread has it's own semaphore in one large semaphore array.\n");
+		printf(" The semaphores are always 0, i.e. the threads never sleep and no task\n");
+		printf(" switching will occur.\n");
+		printf(" This might be representative for a big-reader style lock.\n");
+		return 1;
+	}
+	timeout = atoi(argv[1]);
+
+	if (argc > 3) {
+		cpus = decode_commastring(argv[3]);
+		i=1;
+		g_max_cpus = cpus[0];
+		while(cpus[i] != 0) {
+			if (cpus[i] > g_max_cpus)
+				g_max_cpus = cpus[i];
+			i++;
+		}
+	} else {
+		cpu_set_t cpuset;
+		int ret;
+
+		ret = pthread_getaffinity_np(pthread_self(), sizeof(cpuset), &cpuset);
+		if (ret < 0) {
+			printf("pthread_getaffinity_np() failed with errno %d.\n", errno);
+			return 1;
+		} else {
+			g_max_cpus = 0;
+			while (CPU_ISSET(g_max_cpus, &cpuset))
+				g_max_cpus++;
+		}
+		if (g_max_cpus == 0) {
+			printf("Autodetection of the number of cpus failed.\n");
+			return 1;
+		}
+		j = 1;
+		i = 0;
+		while (j < g_max_cpus) {
+			j+=j*0.2+1;
+			i++;
+		}
+		i = i + 2;
+		cpus = (int*)malloc(sizeof(int)*(i+2));
+		if (!cpus) {
+			printf("Could not allocate memory for decoding parameters.\n");
+			exit(1);
+		}
+		j = 1;
+		i = 0;
+		while (j < g_max_cpus) {
+			cpus[i] = j;
+			j+=j*0.2+1;
+			i++;
+		}
+		cpus[i] = g_max_cpus;
+		cpus[i+1] = 0;
+	}
+	if (argc > 2) {
+		interleaves = decode_commastring(argv[2]);
+	} else {
+		j=g_max_cpus-1;
+		if (j==0)
+			j = 1;
+
+		i = 0;
+		while (j > 0) {
+			j = j/2;
+			i++;
+		}
+		interleaves = (int*)malloc(sizeof(int)*(i+1));
+		if (!interleaves) {
+			printf("Could not allocate memory for decoding parameters.\n");
+			exit(1);
+		}
+		for (j = 0; j < i; j++)
+			interleaves[j] = 1<<j;
+		interleaves[i] = 0;
+	}
+#ifdef VERBOSE
+	for (k = 0; interleaves[k] != 0; k++) {
+		printf("  Interleave %d: %d.\n", k, interleaves[k]);
+	}
+	for (k = 0; cpus[k] != 0; k++) {
+		printf("  Cpu count %d: %d.\n", k, cpus[k]);
+	}
+#endif
+	for (k = 0; interleaves[k] != 0; k++) {
+		for (j=0;;) {
+			max_totals = 0;
+			fastest = 0;
+			for (i=0; cpus[i] != 0; i++) {
+				totals = do_psem(cpus[i], timeout, j, interleaves[k]);
+				if (totals > max_totals) {
+					max_totals = totals;
+					fastest = cpus[i];
+				} else {
+					if (totals < 0.5*max_totals && cpus[i] > 1.5*fastest)
+						break;
+				}
+			}
+			printf("Interleave %d, delay %d: Max total: %lld with %d cpus\n",
+					interleaves[k], j, max_totals, fastest);
+			if (fastest == g_max_cpus)
+				break;
+			/* increase delay in 30% steps */
+			j += j * 0.3 + 1;
+		}
+	}
+}
